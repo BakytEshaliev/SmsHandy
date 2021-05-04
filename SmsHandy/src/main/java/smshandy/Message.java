41dc22134e591cdcf05f81c9287ccc9294725c0c
Justin Nötzold <justin.noetzold.j2r@fh-zwickau.de>
2021-05-04 13:46:14 +0200

    add Javadoc in Provider and Message

--- SmsHandy/src/main/java/smshandy/Message.java
+++ SmsHandy/src/main/java/smshandy/Message.java
@@ -4,77 +4,85 @@ import java.util.Date;
 import java.util.Objects;
 
 public class Message {
-    private String content;
-    private Date date;
-    private String from,to;
 
-    public Message() {
-    }
+	private String content;
+	private Date date;
+	private String from, to;
 
-    public Message(String content,String to, String from, Date date) {
-        if (from == null)
-            throw new IllegalArgumentException("The sender's number must be set");
-        this.content = content;
-        this.date = date;
-        this.from = from;
-        this.to = to;
-    }
+	/**
+	 * Default constructor for Message
+	 */
+	public Message() {
+	}
 
-    @Override
-    public String toString() {
-        return String.format("Content: %s\n" +
-                            "From: %s\n" +
-                            "To: %s\n" +
-                            "Date: %s\n",
-                this.content,this.from,this.to,this.date);
-    }
+	/**
+	 * Constructor for Message with 4 parameters.
+	 * 
+	 * @param content the content of the message
+	 * @param to      the receiver of the message
+	 * @param from    the sender of the message
+	 * @param date    the date when the message is created
+	 */
+	public Message(String content, String to, String from, Date date) {
+		if (from == null)
+			throw new IllegalArgumentException("The sender's number must be set");
+		this.content = content;
+		this.date = date;
+		this.from = from;
+		this.to = to;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	@Override
+	public String toString() {
+		return String.format("Content: %s\n" + "From: %s\n" + "To: %s\n" + "Date: %s\n", this.content, this.from,
+				this.to, this.date);
+	}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public Date getDate() {
-        return date;
-    }
+	public void setContent(String content) {
+		this.content = content;
+	}
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+	public Date getDate() {
+		return date;
+	}
 
-    public String getFrom() {
-        return from;
-    }
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
-    public void setFrom(String from) {
-        this.from = from;
-    }
+	public String getFrom() {
+		return from;
+	}
 
-    public String getTo() {
-        return to;
-    }
+	public void setFrom(String from) {
+		this.from = from;
+	}
 
-    public void setTo(String to) {
-        this.to = to;
-    }
+	public String getTo() {
+		return to;
+	}
 
+	public void setTo(String to) {
+		this.to = to;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Message message = (Message) o;
-        return Objects.equals(content, message.content) &&
-                Objects.equals(date, message.date) &&
-                Objects.equals(from, message.from) &&
-                Objects.equals(to, message.to);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Message message = (Message) o;
+		return Objects.equals(content, message.content) && Objects.equals(date, message.date)
+				&& Objects.equals(from, message.from) && Objects.equals(to, message.to);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(content, date, from, to);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(content, date, from, to);
+	}
 }
--- SmsHandy/src/main/java/smshandy/Provider.java
+++ SmsHandy/src/main/java/smshandy/Provider.java
@@ -1,113 +1,170 @@
 package smshandy;
 
-
 import java.awt.*;
 import java.util.*;
 import java.util.List;
 
 public class Provider {
-    private String name;
-    private Map<String,Integer> credits;
 
-    static List<Provider> providers = new ArrayList<>();
+	private String name;
+	private Map<String, Integer> credits;
 
-    Map<String, SmsHandy> subscriber;
+	static List<Provider> providers = new ArrayList<>();
 
-    public Provider(String name) {
-        this.name = name;
-        credits = new HashMap<>();
-        providers.add(this);
-        subscriber = new HashMap<>();
-    }
+	Map<String, SmsHandy> subscriber;
 
+	/**
+	 * Constructor for Provider.
+	 * 
+	 * @param name the name of the provider
+	 */
+	public Provider(String name) {
+		this.name = name;
+		credits = new HashMap<>();
+		providers.add(this);
+		subscriber = new HashMap<>();
+	}
 
-    public void register(SmsHandy smsHandy){
-        if (smsHandy == null)
-            throw new IllegalArgumentException("The phone must be set");
-        subscriber.put(smsHandy.getNumber(), smsHandy);
-    }
-    public void deposit(String number, int amount){
-        if(number == null)
-            throw new IllegalArgumentException("The number must be set");
-        if (credits.containsKey(number)) {
-            Integer balance = credits.get(number);
-            credits.put(number, balance + amount);
-        }else {
-            credits.put(number, amount);
-        }
-    }
-    public boolean send(Message message){
-        if (message == null)
-            throw new IllegalArgumentException("The message must be set");
+	/**
+	 * Add the smsHandy to the map subscriber, when the smsHandy isn't null.
+	 * 
+	 * @param smsHandy the smsHandy that is to be registered
+	 */
+	public void register(SmsHandy smsHandy) {
+		if (smsHandy == null)
+			throw new IllegalArgumentException("The phone must be set");
+		subscriber.put(smsHandy.getNumber(), smsHandy);
+	}
 
+	/**
+	 * Increase the balance from the number.
+	 * 
+	 * @param number the number which buy new credits
+	 * @param amount the bought credits
+	 */
+	public void deposit(String number, int amount) {
+		if (number == null)
+			throw new IllegalArgumentException("The number must be set");
+		if (credits.containsKey(number)) {
+			Integer balance = credits.get(number);
+			credits.put(number, balance + amount);
+		} else {
+			credits.put(number, amount);
+		}
+	}
 
-        String sender = message.getFrom();
-        String receiver = message.getTo();
+	/**
+	 * Send the message, pay for the message and return true, if is possible to send
+	 * this message.
+	 * 
+	 * @param message the message to be sent
+	 * @return true, if is possible to send the message, otherwise false
+	 */
+	public boolean send(Message message) {
+		if (message == null)
+			throw new IllegalArgumentException("The message must be set");
 
-        SmsHandy senderHandy = subscriber.get(sender);
-        Provider providerFor = findProviderFor(receiver);
+		String sender = message.getFrom();
+		String receiver = message.getTo();
 
-        if(senderHandy.getProvider() != null &&  providerFor != null){
-            senderHandy.addToSentMessages(message);
-            SmsHandy receiverHandy = providerFor.subscriber.get(receiver);
-            receiverHandy.receiveSms(message);
-            senderHandy.payForSms();
+		SmsHandy senderHandy = subscriber.get(sender);
+		Provider providerFor = findProviderFor(receiver);
 
-            return true;
-        }
-        return false;
-    }
+		if (senderHandy.getProvider() != null && providerFor != null) {
+			senderHandy.addToSentMessages(message);
+			SmsHandy receiverHandy = providerFor.subscriber.get(receiver);
+			receiverHandy.receiveSms(message);
+			senderHandy.payForSms();
 
+			return true;
+		}
+		return false;
+	}
 
-    public int getCreditForSMsHandy(String number){
-        return credits.getOrDefault(number,0);
-    }
+	/**
+	 * Return the balance from the number.
+	 * 
+	 * @param number the to checked number
+	 * @return the balance or 0, if the number isn't in the map
+	 */
+	public int getCreditForSMsHandy(String number) {
+		return credits.getOrDefault(number, 0);
+	}
 
-    private boolean canSendTo(String receiver){
-        return subscriber.containsKey(receiver);
-    }
+	/**
+	 * Checks, if the receiver is in the map subscriber.
+	 * 
+	 * @param receiver the to checked receiver
+	 * @return return true, if the receiver is in the map, otherwise false.
+	 */
+	private boolean canSendTo(String receiver) {
+		return subscriber.containsKey(receiver);
+	}
 
-    private static Provider findProviderFor(String receiver){
-        for (Provider provider : providers) {
-            if (provider.canSendTo(receiver)) {
-                return provider;
-            }
-        }
-        return null;
-    }
+	/**
+	 * Search the provider for the receiver.
+	 * 
+	 * @param receiver the receiver, that is checked for the provider
+	 * @return the provider from the receiver
+	 */
+	private static Provider findProviderFor(String receiver) {
+		for (Provider provider : providers) {
+			if (provider.canSendTo(receiver)) {
+				return provider;
+			}
+		}
+		return null;
+	}
 
-    public Map<String, Integer> getCredits() {
-        return credits;
-    }
+	/**
+	 * Return the map credits.
+	 * 
+	 * @return the map credits
+	 */
+	public Map<String, Integer> getCredits() {
+		return credits;
+	}
 
-    public String getName() {
-        return name;
-    }
+	/**
+	 * Return the name of the provider.
+	 * 
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Provider provider = (Provider) o;
-        return Objects.equals(name, provider.name) &&
-                Objects.equals(credits, provider.credits) &&
-                Objects.equals(subscriber, provider.subscriber);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Provider provider = (Provider) o;
+		return Objects.equals(name, provider.name) && Objects.equals(credits, provider.credits)
+				&& Objects.equals(subscriber, provider.subscriber);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, credits, subscriber);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, credits, subscriber);
+	}
 
-    public Map<String, SmsHandy> getSubscriber() {
-        return subscriber;
-    }
+	/**
+	 * Return the subscriber map.
+	 * 
+	 * @return the map subscriber
+	 */
+	public Map<String, SmsHandy> getSubscriber() {
+		return subscriber;
+	}
 
-    public void setSubscriber(Map<String, SmsHandy> subscriber) {
-        this.subscriber = subscriber;
-    }
-    public void setCredits(Map<String, Integer> credits) {
-        this.credits = credits;
-    }
+	public void setSubscriber(Map<String, SmsHandy> subscriber) {
+		this.subscriber = subscriber;
+	}
+
+	public void setCredits(Map<String, Integer> credits) {
+		this.credits = credits;
+	}
 
 }
