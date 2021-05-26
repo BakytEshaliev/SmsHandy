7304735f489e594607054380c853f02ea8688341
Justin Nötzold <justin.noetzold.j2r@fh-zwickau.de>
2021-05-26 20:44:03 +0200

    add a function to delete a Provider

--- SmsHandy/src/main/java/smshandy/controller/ProviderController.java
+++ SmsHandy/src/main/java/smshandy/controller/ProviderController.java
@@ -10,18 +10,26 @@ import smshandy.Provider;
 
 public class ProviderController {
 
-    @FXML
-    private TableView<Provider> providerTable;
-    @FXML
-    private TableColumn<Provider,String> providerCol;
+	@FXML
+	private TableView<Provider> providerTable;
+	@FXML
+	private TableColumn<Provider, String> providerCol;
 
-    DBinit db = DBinit.getInstance();
-    public void initialize(){
-        loadAllProviders();
-        providerCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getName()));
-    }
+	DBinit db = DBinit.getInstance();
 
-    private void loadAllProviders(){
-        providerTable.setItems(FXCollections.observableArrayList(db.getAllProviders()));
-    }
+	public void initialize() {
+		loadAllProviders();
+		providerCol.setCellValueFactory(cell -> new SimpleStringProperty(cell.getValue().getName()));
+	}
+
+	private void loadAllProviders() {
+		providerTable.setItems(FXCollections.observableArrayList(db.getAllProviders()));
+	}
+
+	public void deleteProviderBtn() {
+		int selectedIndex = providerTable.getSelectionModel().getSelectedIndex();
+		if (selectedIndex >= 0) {
+			providerTable.getItems().remove(selectedIndex);
+		}
+	}
 }
